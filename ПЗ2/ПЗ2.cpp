59816087a7821002f723c67411bc4d983e3573b1
Victoria-Mykhailneko <vickysya2018@gmail.com>
2025-09-08 12:07:15 +0300

    Add project files.

---
+++ ПЗ2/ПЗ2.cpp
@@ -0,0 +1,32 @@
+﻿// ПЗ2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    double a, b, c, r, s, p;
+    cout << "Введіть a, b, c" << endl;
+    cin >> a >> b >> c;
+
+    p = (a + b + c) / 2;  // півпериметр
+    s = sqrt(p * (p - a) * (p - b) * (p - c));  // площа за формулою Герона
+    r = s / p;  // радіус вписаного кола
+
+    cout << "r = " << r << endl;
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
